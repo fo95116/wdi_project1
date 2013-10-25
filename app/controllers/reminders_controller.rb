dd0c9cced4e53ba19df089d0778cb4e34bc7b50e
fo95116 <fo95116@gmail.com>
2013-10-25 09:04:41 -0700

    working styled version

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -21,7 +21,7 @@ auth_token = '268b5410875cbf5edd5b1b4c17982c2d'
 #the @client initializes a helper library that will be able to receive and send messages
 #the @client will communicate with twilio's REST API (talks to the twilio server to send messages)
 
-message = @client.account.sms.messages.create(:body => "Success! This is money!",
+message = @client.account.sms.messages.create(:body => "Thank You for using CalendaRx. You will receive a daily reminder at noon to make sure you stay healthy.",
 
     :to => "+1#{phone_number}",
     :from => "+14085123430")
